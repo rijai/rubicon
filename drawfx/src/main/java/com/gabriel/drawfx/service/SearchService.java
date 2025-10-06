64b673e236f581c4f44a2ae31d1303b63092ed77
Leo Gabriel <leo.gabriel1@assaabloy.com>
2025-10-06 08:30:28 +0800

    addedd search

---
+++ drawfx/src/main/java/com/gabriel/drawfx/service/SearchService.java
@@ -0,0 +1,23 @@
+package com.gabriel.drawfx.service;
+
+
+import com.gabriel.drawfx.model.Shape;
+
+import java.awt.*;
+import java.util.List;
+public class SearchService {
+    public void search(AppService appService, Point p){
+        appService.getDrawing().getSelectedShapes().clear();
+        List<Shape> shapes = appService.getDrawing().getShapes();
+        int r = appService.getSearchRadius();
+        for(Shape shape : shapes){
+            Point loc = shape.getLocation();
+            int width = shape.getWidth();
+            int height = shape.getHeight();
+            if(p.x > loc.x-r && p.x < loc.x+ width+r && p.y > loc.y -r && p.y < loc.y+ height + r){
+                appService.getDrawing().getSelectedShapes().add(shape);
+
+            }
+        }
+    }
+}
